16482e3f6a0e99957d8b7022dfbc56ad68be6569
Changmin-Hyeon <hcm7318@naver.com>
2016-09-21 20:21:24 +0900

    c파일최종안

--- HW1_6조/현창민_2011104349/HW_2011104349_현창민.c
+++ HW1_6조/현창민_2011104349/HW_2011104349_현창민.c
@@ -9,21 +9,21 @@ int index[100], cnt=0, indexcnt=0;
 //노드 구조
    typedef struct node
    {
-      char word[20];
+      char word[20];	//문자열을 데이터로 저장하는 노드
       struct node *next;
    }node;
 
 //노드 생성   
    node *newnode(char word[20])
    {
-      node *newnode = (node*)malloc(sizeof(node));
-      strcpy(newnode->word, word);
+      node *newnode = (node*)malloc(sizeof(node));	//node* 변수를 malloc을 이용해 동적메모리 할당
+      strcpy(newnode->word, word);					//포인터 이용해 입력변수를 저장
       newnode->next=NULL;
       return newnode;
    }
 
 //노드간 연결
-   void Linknode(node* h, node* n)
+   void Linknode(node* h, node* n)	//새로운 노드가 첫번째 노드일 경우 헤더 노드의 next가 가르키는 주소에 노드 지정
    {
       node* p = h->next;
       if(h->next == NULL)
@@ -32,7 +32,7 @@ int index[100], cnt=0, indexcnt=0;
       }
       else
       {
-         while(p->next != NULL)
+         while(p->next != NULL)		//새로운 노드가 첫번째가 아닐시 각 노드의 next를 타고 마지막 노드의 next가 가르키는 주소에 노드 지정
          {
             p=p->next;
          }
@@ -40,7 +40,7 @@ int index[100], cnt=0, indexcnt=0;
       }
    }
 
-   node *Findnode(node* h, char fword[20])
+   node *Findnode(node* h, char fword[20])	//입력 변수로 node*형 변수와 검색할 단어를 받는다.
    {
       node* p = h->next;
       
@@ -58,38 +58,42 @@ int index[100], cnt=0, indexcnt=0;
 
    //파일 불러와서 노드 만드는 함수
 
+
    node *makenode(void)
    {
-	   node* h = (node*)malloc(sizeof(node));
-      node* s;
-      int i;
+	   node* h = (node*)malloc(sizeof(node));	//포인터로 해주지 않을시 함수가 끝난후 메모리가 남아있지 않게 된다.
+       node* s;
+       int i,k;
 	  
-      char buf[20], find[20];
-      FILE *fileopen;
+       char buf[20], find[20];
+       FILE *fileopen;
 
-      h->word[20] = 0;
-      h->next = NULL;
+       h->word[20] = 0;
+       h->next = NULL;
 
-      fileopen = fopen("a.txt","rt");
+       fileopen = fopen("a.txt","rt");
 
-      while(fscanf(fileopen, "%s\n", &buf) != EOF)
-      {
-		  for(i=0; i<sizeof(buf); i++)
-		  {
-			  if((buf[i]==33)||(buf[i]==34)||(buf[i]==39)||(buf[i]==44)||(buf[i]==46)||(buf[i]==63)) //특수문자에 널값 넣어서 삭제
+       while(fscanf(fileopen, "%s\n", &buf) != EOF)
+       {
+	 	  for(i=0; i<sizeof(buf); i++) //for문으로 문자열 전체에 특수문자가 있는지 확인
+	 	  {
+			  while((buf[i]==33)||(buf[i]==34)||(buf[i]==39)||(buf[i]==44)||(buf[i]==46)||(buf[i]==63)) //특수문자가 없어질때까지 반복
 			  {
-				  buf[i]=NULL;
-
-			  };
+				  if((buf[i]==33)||(buf[i]==34)||(buf[i]==39)||(buf[i]==44)||(buf[i]==46)||(buf[i]==63)) //특수문자에 널값 넣어서 삭제
+	 			  {
+					  for(k=i;buf[k]!=0;k++)
+						  buf[k]=buf[k+1];
+				  };
+			  }
 		  };
           printf("%s\n",&buf);
           s = newnode(buf);
           Linknode(h,s);
-      }
+       }
       
-      fclose(fileopen);
-	  return h;
-   }
+       fclose(fileopen);
+	   return h;
+    }
 
 
    //메인함수
